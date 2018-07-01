c89c0c50f16b7f1673665111ba6f0dc036cb0eeb
Anonta <SH-anonta@users.noreply.github.com>
2018-07-01 18:12:16 +0600

    Remove temp component includes in root router module Add link to deployed prototype in the readme file

--- src/app/root-router.module.ts
+++ src/app/root-router.module.ts
@@ -8,16 +8,14 @@ import {LoginComponent} from './page-components/login/login.component';
 import {RegisterComponent} from './page-components/register/register.component';
 import {DfaCreatorComponent} from './feature-modules/automata-creator/dfa-creator/dfa-creator.component';
 import {AutomataCreatorModule} from './feature-modules/automata-creator/automata-creator.module';
-import {TempComponent} from './temp/temp.component';
 
 
 
-let routes = [
+const routes = [
   {path: '', component: HomepageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'dfa', component: DfaCreatorComponent},
-  {path: 'temp', component: TempComponent},   // todo remove, only for experimental purposes
 
   {path: 'error-404', component: Error404Component},
   {path: '**', redirectTo: 'error-404'},
@@ -29,7 +27,6 @@ let routes = [
     Error404Component,
     LoginComponent,
     RegisterComponent,
-    TempComponent     // todo remove, only for experimental purposes
   ],
   imports: [
     RouterModule.forRoot(routes),
