9da96241cd8644691660d869ebdb8d03f570dd20
Anonta <SH-anonta@users.noreply.github.com>
2018-07-07 13:55:01 +0600

    Implement contest details UI

--- src/app/page-components/contest/contest-details/contest-details.component.ts
+++ src/app/page-components/contest/contest-details/contest-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../../global-services/user.service';
 
 @Component({
   selector: 'app-contest-details',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContestDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public user_service: UserService) { }
 
   ngOnInit() {
   }
