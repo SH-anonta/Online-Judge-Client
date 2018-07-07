bdaa135a1a1e53e299ab2952423ab02a57f86461
Anonta <SH-anonta@users.noreply.github.com>
2018-07-07 18:39:00 +0600

    Implement contest submit UI

--- src/app/page-components/contest/contest-submit/contest-submit.component.ts
+++ src/app/page-components/contest/contest-submit/contest-submit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contest-submit',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContestSubmitComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
+  submitBtnClickHandler() {
+    this.router.navigate(['/contest/3/user/3/submissions'])
+  }
 }
