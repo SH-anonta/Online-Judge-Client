0b37d043b80bc75d3bfd656018834234c792432e
Anonta <SH-anonta@users.noreply.github.com>
2018-07-07 22:11:11 +0600

    Modify sidebar Admin page links are shown only if user is an admin

--- src/app/shared-components/sidebar/sidebar.component.ts
+++ src/app/shared-components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../global-services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public user_service: UserService) { }
 
   ngOnInit() {
   }
