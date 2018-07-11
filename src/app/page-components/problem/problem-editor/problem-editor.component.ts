e7d24c91c9c9273a779d95e20b33f7cdb89b3195
Anonta <SH-anonta@users.noreply.github.com>
2018-07-11 23:54:17 +0600

    Modify problem-editor component - Convert 4space tabs to 2space tabs - Restructure form elements - Add two form fields

--- src/app/page-components/problem/problem-editor/problem-editor.component.ts
+++ src/app/page-components/problem/problem-editor/problem-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-problem-editor',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProblemEditorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onSaveBtnClick() {
+    this.router.navigate(['/problem/12']);
+  }
 }
