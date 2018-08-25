ddc0d8ccfe4d5b92c86e6fa6d7a85705a40a171f
Anonta <SH-anonta@users.noreply.github.com>
2018-08-25 17:19:37 +0600

    Implement count-down component

--- src/app/page-components/contest/contest-count-down/contest-count-down.component.ts
+++ src/app/page-components/contest/contest-count-down/contest-count-down.component.ts
@@ -2,7 +2,10 @@
 // users get redirected here if they try to access contest problems before the contest begins
 
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ContestDetailsData, ContestRepositoryService} from '../../../global-services/repository-services/contest-repository.service';
+import {LinkGeneratorService} from '../../../global-services/link-generator.service';
+import {UserService} from '../../../global-services/user.service';
 
 @Component({
   selector: 'app-contest-count-down',
@@ -10,9 +13,20 @@ import {Router} from '@angular/router';
   styleUrls: ['./contest-count-down.component.css']
 })
 export class ContestCountDownComponent implements OnInit{
-  contest_start_time: Date;
+  show_count_down:boolean = false;
 
-  constructor(private router: Router) {
+  contest_start_time: Date;
+  contest_id: number;
+  contest_data: ContestDetailsData = new ContestDetailsData();
+
+  constructor(public user_service: UserService,
+              public route: ActivatedRoute,
+              public link_generator: LinkGeneratorService,
+              public contest_repository: ContestRepositoryService,
+              private router: Router) {
+
+    this.contest_id = this.route.snapshot.params['contest_id'];
+
     // set contest time to 15s in the future
     let time = new Date();
     this.contest_start_time= new Date();
@@ -20,10 +34,23 @@ export class ContestCountDownComponent implements OnInit{
   }
 
   ngOnInit() {
+    this.loadData();
   }
 
+
+  loadData(){
+    let promise = this.contest_repository.getContestDetails(this.contest_id);
+
+    promise.then(data => {
+      this.contest_data = data;
+      this.contest_start_time= new Date(data.StartDate);
+      this.show_count_down= true;
+    });
+  }
+
+
   onCountDownComplete(){
-    this.router.navigate(['/contest/12/problems']);
+    // this.router.navigate(this.link_generator.contestProblemList(this.contest_id));
   }
 
 }
